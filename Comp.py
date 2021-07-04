c850a5657c6a7081b96e3d1d6a8b339a125698bd
EulerMacaroni <itsm0u@my.yorku.ca>
2021-07-04 00:32:53 -0400

    update

--- Comp.py
+++ Comp.py
@@ -2,31 +2,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 from TOV import TOV
 from functions import rel, mass
+from TOV2step import TOV2step
 
-# eps = np.finfo(float).eps
-# P = np.linspace(1e-5,1e3,1000)
-# rho = 261            # (107-108)
-# r_m = np.array([])
-# for i in range(len(P)):
-#     sol = TOV(P[i],rho)
-#     r_m = np.append(r_m,sol[5])
+eps = np.finfo(float).eps
+P = np.linspace(1e-5,1e5,100)
+rho = 5000           # (107-108)
+r_m = np.array([])
+for i in range(len(P)):
+    print(i)
+    sol = TOV(P[i],rho)
+    r_m = np.append(r_m,sol[5])
+    if sol[5] <= 9/4:
+        print('Illegal')
 
-# plt.ylabel('Central Pressure, $P_0$')
-# plt.xlabel('R/M')
-# plt.xlim([0,20])
-# plt.title('Central Pressure vs R/M with rho = %f'%rho)
-# plt.axvline(x=9/4,label='Buchdahl limit',color='green',linestyle = 'dashed')
-# plt.plot(r_m,P)
-# plt.show()
+plt.ylabel('Central Pressure, $P_0$')
+plt.xlabel('R/M')
+plt.xlim([0,20])
+plt.title('Central Pressure vs R/M with rho = %f'%rho)
+plt.axvline(x=9/4,label='Buchdahl limit',color='green',linestyle = 'dashed')
+plt.plot(r_m,P)
+plt.show()
 
 # h = 1000
 # # rho = 1e4
 # while True:
-#     P = 1e6
+#     P = 1e9
 #     rho = 1000+h
 #     print('P0 = ',P,'and rho=',rho)
 #     sol = TOV(P,rho)
-#     if sol[5] <= 2.25:
+#     if sol[5] <= 2.24:
 #         print('illegal star')
-#         # break
-#     h += 1000
+#         break
+#     h += 1e4
+
+
+
+# rho = 1
+
+# # sol1 = TOV2step(1,rho,2.25/2)
+# sol = TOV(1,rho)
+# plt.yscale('log')
+# plt.plot(sol[2],sol[3])
+# print('mass calculated=', mass(sol[0],rho))
+# print('m_array',sol[4])
+# print('sum=',np.sum(sol[4]))
+# plt.show()
+
+# rho = 100
+# sol = TOV(1e3,rho)
+# R = sol[0]
+# M = sol[1]
+# r_arr = sol[2]
+
+# m_arr = sol[4]
+# P = sol[3]
+# M2 = mass(R,rho)
+# P1 = rel(r_arr,R,M,rho)
+# print('analytical mass=',M2, 'and num =',M[0])
+# # print('P=',P)
+# plt.plot(r_arr,P)
+# # print('r=',r_arr)
+# plt.plot(r_arr,P1)
+# plt.legend(['num','exact'])
+# plt.show()
+
+
--- TOV2step.py
+++ TOV2step.py
@@ -6,14 +6,20 @@ from functions import rel, mass
 
 # single density profile TOV solver
 
-def TOV(P,rho):
+def TOV2step(P,rho,r_c):
+
+    def rhof(r):
+        if r <= r_c:
+            return rho
+        else: 
+            return rho/2 
 
     def dm_dr(r,M):
-        return (4)*(np.pi)*(r**2)*rho_0
+        return (4)*(np.pi)*(r**2)*rhof(r)
 
     def dP_dr(r,P):
-        k  = (mstep*rho_0)/(r**2)
-        k1 = 1 + (P/rho_0)
+        k  = (mstep*rho(r))/(r**2)
+        k1 = 1 + (P/rho(r))
         k2 = 1 + ((4*np.pi*(r**3)*P)/(mstep))
         k3 = (1 - ((2*mstep)/(r)))**(-1)
         dPdr = -(k)*(k1*k2*k3)
@@ -24,9 +30,8 @@ def TOV(P,rho):
     rho_0 = rho 
     P0 = P
     
-    r_new = 1e-4
+    r_new = 1e-6 
     m0 = eps
-    t_span =np.linspace(r_new,1e-4+r_new,10)
     
     # # solving Mass and Pressure differential equations
 
@@ -38,26 +43,24 @@ def TOV(P,rho):
 
     while True:
     
+        t_span = [r_new,(1.001)*r_new]
+        # t_span = [r_new+eps]
+        
         m = odeint(dm_dr,m0,t_span,tfirst=True)
-        
         mstep = m[-1]
-
         Pressure = odeint(dP_dr,P0,t_span,tfirst=True)
-
-        
         P_array = np.append(P_array,Pressure[-1])
         r_array = np.append(r_array,t_span[-1])
         m_array = np.append(m_array,m[-1])
-        
-        
+        # print(Pressure[-1])
+
         if Pressure[-1] <= 1e-10:
             R = r_array[-1]
             M = m_array[-1]
             print('Star found with R= ',R,'& M=',M, 'Compactness(R/M > 2.25) = ', (R)/(M))
             break
-        
 
-        t_span = np.linspace(r_array[-1],1e-4+r_array[-1],10)
+        r_new += 1e-5   
     
         P0 = Pressure[-1]
         m0 = m[-1]
