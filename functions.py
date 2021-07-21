a4c4c854aefc3a51970405bd8374e0a98cdf11bf
Alexander Brisebois <alegbrisebois7@gmail.com>
2021-07-21 08:20:27 -0400

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,85 @@
+import numpy as np
+pi = np.pi
+
+# mass function
+def mass(r,rho_0):
+    return (4/3)*(np.pi)*(r**3)*(rho_0)
+
+# Mass Function
+def Mass(R,rho_0):
+    return mass(R,rho_0)
+
+# Non relativistc solution
+def Nrel(y0,r,R):
+     return y0*(1 - r**2/R**2)
+
+# relativistic solution
+def rel(r,R,M,rho_0):
+    A = (R*np.sqrt(R-2*M)-np.sqrt((R**3)-2*M*(r**2)))
+    B = (np.sqrt((R**3)-2*M*(r**2))-3*R*np.sqrt(R-2*M)) 
+    return (rho_0)*(A/B)
+
+def y0(m_r,rho_0):
+    
+    return rho_0*((np.sqrt(1-2*m_r) -1)/(1-3*np.sqrt(1-2*m_r)))
+
+
+# converstion energy density (SI --> Geom.)
+
+def convED(rho_0):
+    return (rho_0*(6.6743*(10**-11)))/((2.9979*(10**8))**4)
+
+# converstion mass (SI --> Geom.)
+def convM(M):
+    return (M*(6.6743*(10**-11)))/((2.9979*(10**8))**2)
+
+def convP(P):
+    return (P*(6.6743*(10**-11)))/((2.9979*(10**8))**4)
+
+
+# funtion for equation of state 
+
+def EosRho(k):
+    m_f =1
+    return (1/(pi**2))*(k**2)*(np.sqrt(m_f**2 + k**2))
+    
+def EosP(k):
+    m_f =1
+    return (1/(3*pi**2))*((k**4)/(np.sqrt(m_f**2 + k**2)))
+
+def intTerm(y,z):
+    return (((1/(3*pi**2)))**2)*(y**2)*(z**6)
+
+def findXPoint(x1,x2,y1,y2,y3):
+    m = (x2 - x1)/(y2 - y1)
+    x3 = (y3 - y1)*m + x1
+    return x3
+
+def minP(P_value):
+    if P_value <= 1e-7:
+        return P_value*(1e-3)
+    else: 
+        return 1e-10
+
+
+def tauf1(P0):
+    if P0 <= 1e-7:
+        return 0.01
+    elif P0> 1e-7 and P0 <= 1e-6:
+        return 0.01
+    elif P0> 1e-6 and P0 <= 1e-5:
+        return 0.01
+    elif P0> 1e-5 and P0 <= 1e-4:
+        return 0.01
+    elif P0 > 1e-4 and P0 <= 1e-3:
+        return 0.01/2
+    elif P0 > 1e-3 and P0 <= 1e-2:
+        return 0.001
+    elif P0 > 1e-2 and P0 <= 1e-0:
+        return 0.0005
+    elif P0 > 1e-0 and P0 <= 1e2:
+        return 0.0001
+    elif P0 > 1e2 and P0 <= 1e3:
+        return 0.00001
+    else:
+        return P0*(1e-8)
---
+++ interPwEoS.py
@@ -0,0 +1,80 @@
+import re
+import numpy as np
+from EoS import EoSclass,EoSIntClass
+from functions import findXPoint
+from scipy.interpolate import interp1d
+
+E = EoSclass
+def rhof(P):            # Pressure on x-axis and Rho on y-axis
+    i = min(E.EoSP,key=lambda x:abs(x-P)) #find closest value to P0
+    a = np.where(E.EoSP==i) # index of closest point to P0
+    index =a[0][0] #index of closest P0 (a outputs 2 dim. array)
+    p1 = E.EoSP[index]
+    rho1 = E.EoSrho[index]
+
+    if P > E.EoSP[index]:
+        p2 = E.EoSP[index+1]
+        rho2 = E.EoSrho[index+1]
+        f1 = findXPoint(rho1,rho2,p1,p2,P)
+        # f2 = interp1d([p1,p2],[rho1,rho2])
+
+    elif P < E.EoSP[index]:
+        p2 = E.EoSP[index-1]
+        rho2 = E.EoSrho[index-1]
+        f1 = findXPoint(rho2,rho1,p2,p1,P)
+        # f2 = interp1d([p2,p1],[rho2,rho1])
+    elif P == E.EoSP[index]:
+        f1 = E.EoSrho[index]
+
+    return f1
+
+
+def Pforrho(rho):
+    i = min(E.EoSrho,key=lambda x:abs(x-rho)) #find closest value to P0
+    a = np.where(E.EoSrho==i) # index of closest point to P0
+    index =a[0][0] #index of closest P0 (a outputs 2 dim. array)
+
+    p1 = E.EoSP[index]
+    rho1 = E.EoSrho[index]
+
+    if rho > E.EoSrho[index]:
+        p2 = E.EoSP[index+1]
+        rho2 = E.EoSrho[index+1]
+        f = findXPoint(p1,p2,rho1,rho2,rho)
+        # f2 = interp1d([p1,p2],[rho1,rho2])
+
+    elif rho < E.EoSrho[index]:
+        p2 = E.EoSP[index-1]
+        rho2 = E.EoSrho[index-1]
+        f = findXPoint(p2,p1,rho2,rho1,rho)
+
+    elif rho == E.EoSrho[index]:
+        f = E.EoSP[index]
+
+    return f
+
+
+def rhofint(P,y):            # Pressure on x-axis and Rho on y-axis
+
+    EI = EoSIntClass(y)
+    i = min(EI.P,key=lambda x:abs(x-P)) #find closest value to P0
+    a = np.where(EI.P==i) # index of closest point to P0
+    index =a[0][0] #index of closest P0 (a outputs 2 dim. array)
+    p1 = EI.P[index]
+    rho1 = EI.rho[index]
+
+    if P > EI.P[index]:
+        p2 = EI.P[index+1]
+        rho2 = EI.rho[index+1]
+        f1 = findXPoint(rho1,rho2,p1,p2,P)
+        # f2 = interp1d([p1,p2],[rho1,rho2])
+
+    elif P < EI.P[index]:
+        p2 = EI.P[index-1]
+        rho2 = EI.rho[index-1]
+        f1 = findXPoint(rho2,rho1,p2,p1,P)
+        # f2 = interp1d([p2,p1],[rho2,rho1])
+    elif P == EI.P[index]:
+        f1 = EI.rho[index]
+
+    return f1
